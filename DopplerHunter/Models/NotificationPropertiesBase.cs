ab25643ce0e87de4a230a16f14123be4b830dfb6
apoc-master <158763930+apoc-master@users.noreply.github.com>
2026-08-22 19:53:52 -0600

    Refactor property change notification system
    - Introduce NotificationPropertiesBase for INotifyPropertyChanged
    - Update FileMetadata to inherit base and notify on IsSelected
    - Add SelectionChanged event to FileMetadata
    - Refactor BaseViewModel to use NotificationPropertiesBase
    - Update MainViewModel for new notification system and add SelectedFilesCount
    - Bind DELETE ALL SELECTED DUPLICATES button to SelectedFilesCount in XAML

---
+++ DopplerHunter/Models/NotificationPropertiesBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace DopplerHunter.Models
+{
+    public class NotificationPropertiesBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
